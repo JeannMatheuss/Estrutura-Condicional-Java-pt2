a776f4e38f13f183773d519d291f92cca7b96a5f
Jean <jeanmatheus0612@gmail.com>
2024-12-09 20:16:33 -0300

    Descobrindo o Maior dos numeros digitados

--- src/ExemploCalculadora.java
+++ src/ExemploCalculadora.java
@@ -11,19 +11,28 @@ public class ExemploCalculadora {
         A = entrada_dado.nextInt();
         System.out.println("Digite o segundo valor: ");
         B = entrada_dado.nextInt();
+        System.out.println("Escolha um operador(+, -, *, /): ");
         s = entrada_dado.next();
             op = s.charAt(0); 
 
             switch (op) {
-                case '+': Saida += +A+ " + "+B+" = "+(A+B); break;
-                case '-': Saida += +A+ " - "+B+" = "+(A-B); break;
-                case '*': Saida += +A+ " * "+B+" = "+(A*B); break;
-                case '/': {if (B == 0){
-                    Saida = "A operação não pode ser executada";
-                }
-                else {
-                    Saida += +A+ " / "+B+" = "+(A/B); break;
-                }
+                case '+': 
+                    Saida += +A+ " + "+B+" = "+(A+B); 
+                    break;
+                case '-': 
+                    Saida += +A+ " - "+B+" = "+(A-B); 
+                    break;
+                case '*': 
+                    Saida += +A+ " * "+B+" = "+(A*B); 
+                    break;
+                case '/': 
+                    {if (B == 0){
+                        Saida = "A operação não pode ser executada";
+                    }
+                    else {
+                        Saida += +A+ " / "+B+" = "+(A/B); 
+                        break;
+                    }
             }
                 default: Saida = "Operação invalida";
             }
---
+++ src/MaiorMenor.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class MaiorMenor {
+    public static void main(String[] args) {
+
+        int A, B, C;
+        Scanner valor_digitado = new Scanner (System.in);
+        System.out.println("Digite o primeiro valor: ");
+        A = valor_digitado.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        B = valor_digitado.nextInt();
+        System.out.println("Digite o terceiro valor: ");
+        C = valor_digitado.nextInt();
+
+        System.out.println("Dentre os valores " + A + ", " + B + ", " + C);
+        if (A > B && A > C) {
+            System.out.println("O maior valor é: " + A);
+        }
+        else if (B > A && B > C) {
+            System.out.println("O maior valor é: " + B);
+        }
+        else if (C > A && C > B) {
+            System.out.println("O maior valor é: " + C);
+        }
+        else {
+            System.out.println("Os valores são iguais");
+        }
+
+    }
+}
